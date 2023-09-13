24085b6974ac5ed12bb41be518437ff93394764e
Varun Sai Guda <93998822+varunguda@users.noreply.github.com>
2023-09-14 04:25:18 +0530

    home page initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-// import Footer from './components/layouts/Footer/Footer.jsx';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom"
+
+import Footer from './components/layouts/Footer/Footer.jsx';
 import Navbar from './components/layouts/Navbar/Navbar.jsx';
+import Home from "./components/layouts/Home/Home.jsx";
 
 const App = () => {
   return (
     <div className='main-container'>
-      <Navbar />
-      {/* <Footer /> */}
+      <Router>
+        <Navbar />
+        <div className='content-container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   )
 }
---
+++ frontend/src/components/layouts/Home/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Home.css';
+
+
+const Home = () => {
+    return (
+        <>
+            <div className="banner-container">
+                <div className="banner banner1">
+                    <span class="text1">Your next phone is</span>
+                    <span class="text2">here</span>
+                </div>
+                <div className="banner banner2">
+                    <span class="text1">A shoe.</span>
+                    <span class="text2">that's it!</span>
+                </div>
+                <div className="banner banner3">
+                    <span class="text1">Your style at ManyIn</span>
+                    <span class="text2">Now Trending</span>
+                </div>
+
+                <div className="banner banner4">
+                <span class="text1">Home made better</span>
+                    <span class="text2">Home & kitchen appliances at very low prices</span>
+                </div>
+
+                <div className="banner banner5">
+                    <span class="text1">Fashion,</span>
+                    <span class="text2">in your fist</span>
+                </div>
+                <div className="banner banner6">
+                    <span class="text1">Sale & all</span>
+                </div>
+                <div className="banner banner7">
+                    <span class="text1">Sell with us.</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
